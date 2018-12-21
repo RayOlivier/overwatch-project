b69c478ce19cb3ce14d8cacba73e15cbe5545d85
Ray Olivier <rayolivier@outlook.com>
2018-12-21 16:25:23 -0600

    setup and notes

--- src/views/UserInfo.js
+++ src/views/UserInfo.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
+//general plan for this:
+// find an overwatch api that i like
+//use the server to make axios calls, mostly for practice, maybe adding sessions and shit
+//learn d3 to display graphs of user stats
+//tryout rxjs for events (watch more of the netflix youtube talks)
+//user redux for all state
+
 class UserInfo extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
   render() {
     return (
       <div>
         <h1>profile idk</h1>
+        <input placeholder="Input battletag..." />
       </div>
     );
   }
