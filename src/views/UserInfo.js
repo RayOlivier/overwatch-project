fb19eafc7ecdcfdbca54eaf899368732b6b8101b
Ray Olivier <rayolivier@outlook.com>
2018-12-21 16:18:08 -0600

    more setup

---
+++ src/views/UserInfo.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class UserInfo extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>profile idk</h1>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
